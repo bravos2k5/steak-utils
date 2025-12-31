02f17761d78c1a89097d5b2eab9af2919b808c3e
Bravos <baonq8205@gmail.com>
2025-12-31 14:29:15 +0700

    Fix jitpack build

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,24 @@ plugins {
 group = "com.bravos.steak"
 version = "1.0.5"
 
+publishing {
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/bravos2k5/steak-utils")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+}
+
 repositories {
     mavenCentral()
 }
